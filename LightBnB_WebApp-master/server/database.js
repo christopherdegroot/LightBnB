c59e41bc59d376d4db734291b983097c5318786b
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-10 01:08:34 -0800

    add pool.query for lighthouseBnB users and reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -83,7 +83,24 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  const variables = [`${guest_id}`,`${limit}`]
+  console.log(variables)
+  return pool.query (`
+  SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM properties
+  JOIN reservations ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+  GROUP BY reservations.id, properties.id
+  ORDER BY start_date
+  LIMIT $2;`
+  , variables)
+  .then((result) => {return result.rows})
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 }
 exports.getAllReservations = getAllReservations;
 
